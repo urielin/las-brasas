1a8162063fb7150f7214b197c701f6ba988dde93
urielin <urieljnvp@gmail.com>
2020-02-11 15:35:52 -0500

    modificando navbar dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,9 +7,9 @@
     <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google.">
     <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template, eCommerce dashboard, analytic dashboard">
     <meta name="author" content="ThemeSelect">
-    <title>Dashboard Analytics | Materialize - Material Design Admin Template</title>
-    <link rel="apple-touch-icon" href="assets/images/favicon/apple-touch-icon-152x152.png">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon-32x32.png">
+    <title>Las brasas | Grupo Arinez</title>
+    <!-- <link rel="apple-touch-icon" href="assets/images/favicon/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon-32x32.png"> -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <link href="assets/css/main.css" rel="stylesheet">
@@ -65,44 +65,22 @@
     <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-light sidenav-active-square">
       <div class="brand-sidebar">
         <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="index-2.html">
-        <img class="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8jtUc7a4cG1e8lYJqDa-NQGo6V2jLspu33gfebJW84yWuTeuW" alt="materialize logo" style=" position: relative; top: -20px; height: 60px; width: 150px; ">
+        <img class="hide-on-med-and-down" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ8jtUc7a4cG1e8lYJqDa-NQGo6V2jLspu33gfebJW84yWuTeuW" alt="materialize logo" style=" position: relative; top: -20px; height: 60px; width: 150px; ">
       </div>
       <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="menu-accordion">
-        <li class="active bold">
-          <a class="  waves-effect waves-cyan " href="{{route('usuarios')}}">
-            <i class="material-icons">home</i>
-            <span class="menu-title" data-i18n="Dashboard">Administrador</span>
-          </a>
-        </li>
         <li class="navigation-header">
-          <a class="navigation-header-text">Applications</a>
-          <i class="navigation-header-icon material-icons">more_horiz</i>
-        </li>
-        <li class="bold">
-          <a class="waves-effect waves-cyan " href="./">
-            <i class="material-icons">public</i>
-            <span class="menu-title" data-i18n="Mail">Prosegur</span>
-           </a>
-        </li>
-        <li class="bold">
-          <a class="waves-effect waves-cyan " href="./">
-            <i class="material-icons">insert_chart</i>
-            <span class="menu-title" data-i18n="Chat">Contabilidad</span>
-          </a>
-        </li>
-       <li class="navigation-header">
          <a class="navigation-header-text">SISTEMA DE GESTIÓN </a>
          <i class="navigation-header-icon material-icons">more_horiz</i>
         </li>
-        <li class="bold">
+        <!-- <li class="bold">
           <a class="waves-effect waves-cyan " href="./">
           <i class="material-icons">local_shipping</i>
           <span class="menu-title" data-i18n="User Profile">Contenedores y Camiones</span>
           </a>
-        </li>
+        </li> -->
         <li class="bold">
           <a class="collapsible-header waves-effect waves-cyan " href="JavaScript:void(0)">
-            <i class="material-icons">apps</i>
+            <i class="material-icons">local_shipping</i>
             <span class="menu-title" data-i18n="Misc">Maestro</span>
           </a>
           <div class="collapsible-body">
@@ -146,7 +124,30 @@
               </li>
             </ul>
           </div>
-       </li>
+       </li>  
+        <li class="active bold">
+          <a class="  waves-effect waves-cyan " href="{{route('usuarios')}}">
+            <i class="material-icons">home</i>
+            <span class="menu-title" data-i18n="Dashboard">Administrador</span>
+          </a>
+        </li>
+        <li class="navigation-header">
+          <a class="navigation-header-text">Applications</a>
+          <i class="navigation-header-icon material-icons">more_horiz</i>
+        </li>
+        <li class="bold">
+          <a class="waves-effect waves-cyan " href="./">
+            <i class="material-icons">public</i>
+            <span class="menu-title" data-i18n="Mail">Prosegur</span>
+           </a>
+        </li>
+        <li class="bold">
+          <a class="waves-effect waves-cyan " href="./">
+            <i class="material-icons">insert_chart</i>
+            <span class="menu-title" data-i18n="Chat">Contabilidad</span>
+          </a>
+        </li>
+ 
        </ul>
       <div class="navigation-background"></div><a class="sidenav-trigger btn-sidenav-toggle btn-floating btn-medium waves-effect waves-light hide-on-large-only" href="#" data-target="slide-out"><i class="material-icons">menu</i></a>
     </aside>
