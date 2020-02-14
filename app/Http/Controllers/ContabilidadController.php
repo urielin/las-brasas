915e343d034090d22c4823c992797f82958ee347
Diego <fordiego44@gmail.com>
2020-02-14 16:15:38 -0300

    contabilidad

--- app/Http/Controllers/ContabilidadController.php
+++ app/Http/Controllers/ContabilidadController.php
@@ -80,16 +80,31 @@ public function getOtroRetiro(Request $request)
           if ($request -> ajax()) {
 
 
-                  $depositosDetalle=DB::select("SELECT rd.folio, rd.tipo, op.OPER_DESC, s.SUCU_CODIGO, s.SUCU_NOMBRE, ro.fecha_ingreso, ro.monto, ro.descripcion, ro.cartola_fecha  FROM  dbo.MODULO_RETIROS_INDICE_DETALLE rd
-                            INNER JOIN   dbo.MODULO_RETIROS_INDICE ri on ri.id_retiros_indice = rd.id_retiros_indice
-                             left outer join dbo.MODULO_OTROS_RETIROS_PROSEGUR ro on rd.folio= ro.folio
-                            left outer join dbo.ADM_SUCURSAL s on ro.sucursal =s.SUCU_CODIGO
-                            left outer join dbo.MODULO_TP_OPERACION op on ro.t_oper= op.OPER_COD
-                            WHERE fecha_desde = convert(date,'$request->fecha1') and fecha_hasta = convert(date,'$request->fecha2')");
+                  $depositosDetalle1=DB::select("SELECT rc.folio, rd.tipo, op.OPER_DESC, rc.num_caja, rc.n_deposito, rc.fecha_caja, rc.monto, rc.obs, rc.cartola_fecha
+                                        FROM dbo.MODULO_RETIROS_INDICE ri
+                                        INNER JOIN dbo.MODULO_RETIROS_INDICE_DETALLE rd on ri.id_retiros_indice = rd.id_retiros_indice
+                                        INNER JOIN dbo.MODULO_RETIROS_CAJA rc on rd.folio =rc.folio
+                                        INNER JOIN dbo.MODULO_RETIROS_CAJA_DETALLE rcd on rc.folio=rcd.id_folio
+                                        INNER JOIN dbo.MODULO_TP_OPERACION op on rc.cod_op= op.OPER_COD
+                                        WHERE fecha_desde = convert(date,'$request->fecha1') and fecha_hasta = convert(date,'$request->fecha2') and rd.tipo ='1'");
 
-                  if ($depositosDetalle != null) {
+                  $depositosDetalle2=DB::select("SELECT rd.folio, rd.tipo, op.OPER_DESC, s.SUCU_CODIGO, s.SUCU_NOMBRE, ro.deposito, ro.fecha_ingreso, ro.monto, ro.descripcion, ro.cartola_fecha
+                                        FROM  dbo.MODULO_RETIROS_INDICE_DETALLE rd
+                                        INNER JOIN dbo.MODULO_RETIROS_INDICE ri on ri.id_retiros_indice = rd.id_retiros_indice
+                                        INNER JOIN dbo.MODULO_OTROS_RETIROS_PROSEGUR ro on rd.folio= ro.folio
+                                        INNER JOIN dbo.ADM_SUCURSAL s on ro.sucursal =s.SUCU_CODIGO
+                                        INNER JOIN dbo.MODULO_TP_OPERACION op on ro.t_oper= op.OPER_COD
+                                        WHERE fecha_desde = convert(date,'$request->fecha1') and fecha_hasta = convert(date,'$request->fecha2') and rd.tipo='2'");
+
+
+
+
+                  if ($depositosDetalle2 != null) {
                     return response()->json([
-                        'depositosDetalle'              =>$depositosDetalle
+
+                          'depositosDetalle1'              =>$depositosDetalle1,
+                          'depositosDetalle2'              =>$depositosDetalle2
+                            
                         ]);
 
                   }else {
--- public/js/contabilidad.js
+++ public/js/contabilidad.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $(this).addClass('tr-selected').siblings().removeClass('tr-selected');
   });
 
-})
+});
 
 $(document).ready(function(){
 
@@ -45,15 +45,42 @@ $(document).on('click','#mostrar-detalle',function(){
 
     if($.trim(fecha1) != '' && $.trim(fecha2) != '' ){
 
-          request = $.get('obtener-retiro-detalle',{fecha1:fecha1,fecha2:fecha2 },function(res){
+          $.get('obtener-retiro-detalle',{fecha1:fecha1,fecha2:fecha2 },function(res){
               // $('#retiro-head').empty();
+              console.log('entrooo');
               $('#icono1').empty();
               $('#depositoDetalleHead').empty();
               $('#depositoDetalleTabla').empty();
 
               $('#icono1').append('<div style="display: flex"><i class="material-icons dp48">subject</i><span class="card-title">Detalle Retiro Prosegur</span></div>');
               $('#depositoDetalleHead').append('<tr><th width="6%">Folio</th><th width="10%">Tipo</th><th width="10%">Operación</th><th>Suc</th><th width="10%">Sucursal</th><th width="5%">Caja</th><th>Deposito</th><th width="11%">Fecha</th><th width="7%">Monto</th><th  width="11%">Observacion</th><th width="8%">Cartola</th><th>Acción</th></tr>');
-              $.each(res.depositosDetalle, function(index,value){
+
+
+
+              $.each(res.depositosDetalle1, function(index,value){
+
+                  // let OPER_DESC = value.OPER_DESC == null ? '-' : value.OPER_DESC;
+                  // let SUCU_CODIGO = value.SUCU_CODIGO == null ? '-' : value.SUCU_CODIGO;
+                  // let SUCU_NOMBRE = value.SUCU_NOMBRE == null ? '-' : value.SUCU_NOMBRE;
+                  // let fecha_ingreso = value.fecha_ingreso == null ? '-' : value.fecha_ingreso;
+                  // let monto = value.monto == null ? '-' : value.monto;
+                  // let descripcion = value.descripcion == null ? '-' : value.descripcion;
+                  // let cartola_fecha = value.cartola_fecha == null ? '-' : value.cartola_fecha;
+
+                  if (value.tipo == "1") {
+                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>RETIROS DIARIOS</td><td>'+ value.OPER_DESC +"</td><td> - </td><td> - </td><td>"+ value.num_caja+"</td><td>"+value.n_deposito +"</td><td>"+ value.fecha_caja +" </td><td>"+value.monto +"</td><td>"+ value.obs +"</td><td>"+ value.cartola_fecha +'</td><td><button type="button" value=""   class="editar-gestion  btn red btn-50 darken-1"> <i class="material-icons dp48">close</i></button></td></tr>');
+                  } else {
+                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>OTROS DEPOSITOS</td><td>'+ value.OPER_DESC +"</td><td> - </td><td> - </td><td>"+ value.num_caja+"</td><td>"+value.n_deposito +"</td><td>"+ value.fecha_caja +" </td><td>"+value.monto +"</td><td>"+ value.obs +"</td><td>"+ value.cartola_fecha +'</td><td><button type="button" value=""   class="editar-gestion  btn red btn-50 darken-1"> <i class="material-icons dp48">close</i></button></td></tr>');
+                  }
+                  if (value.monto == null) {
+                    montoTotal+=0;
+                  } else {
+                      montoTotal+=parseFloat(value.monto);
+                  }
+              });
+              // $('#depositoDetalleTabla').append("<tr><td></td><td></td><td></td><td> </td><td></td><td></td><td></td><td></td><td>"+ montoTotal +"</td><td></td><td></td><td></td></tr>");
+
+              $.each(res.depositosDetalle2, function(index,value){
 
                   let OPER_DESC = value.OPER_DESC == null ? '-' : value.OPER_DESC;
                   let SUCU_CODIGO = value.SUCU_CODIGO == null ? '-' : value.SUCU_CODIGO;
@@ -64,9 +91,9 @@ $(document).on('click','#mostrar-detalle',function(){
                   let cartola_fecha = value.cartola_fecha == null ? '-' : value.cartola_fecha;
 
                   if (value.tipo == "1") {
-                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>RETIROS DIARIOS</td><td>'+ OPER_DESC +"</td><td>"+SUCU_CODIGO+" </td><td>"+ SUCU_NOMBRE +"</td><td> - </td><td> - </td><td>"+ fecha_ingreso +" </td><td>"+ monto +"</td><td>"+ descripcion +"</td><td>"+ cartola_fecha +'</td><td><button type="button" value=""   class="editar-gestion  btn red btn-50 darken-1"> <i class="material-icons dp48">close</i></button></td></tr>');
+                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>RETIROS DIARIOS</td><td>'+ OPER_DESC +"</td><td>"+SUCU_CODIGO+" </td><td>"+ SUCU_NOMBRE +"</td><td> - </td><td>"+value.deposito+"</td><td>"+ fecha_ingreso +" </td><td>"+ monto +"</td><td>"+ descripcion +"</td><td>"+ cartola_fecha +'</td><td><button type="button" value=""   class="editar-gestion  btn red btn-50 darken-1"> <i class="material-icons dp48">close</i></button></td></tr>');
                   } else {
-                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>OTROS DEPOSITOS</td><td>'+ OPER_DESC+"</td><td>"+SUCU_CODIGO+" </td><td>"+ SUCU_NOMBRE +"</td><td> - </td><td> - </td><td>"+ fecha_ingreso +" </td><td>"+ monto +"</td><td>"+ descripcion +"</td><td>"+ cartola_fecha +'</td><td><button type="button" value=""  class="editar-gestion  btn red btn-50 darken-1"><i class="material-icons dp48">close</i></button></td></tr>');
+                    $('#depositoDetalleTabla').append('<tr><td>' + value.folio +'</td><td>OTROS DEPOSITOS</td><td>'+ OPER_DESC+"</td><td>"+SUCU_CODIGO+" </td><td>"+ SUCU_NOMBRE +"</td><td> - </td><td>"+value.deposito+"</td><td>"+ fecha_ingreso +" </td><td>"+ monto +"</td><td>"+ descripcion +"</td><td>"+ cartola_fecha +'</td><td><button type="button" value=""  class="editar-gestion  btn red btn-50 darken-1"><i class="material-icons dp48">close</i></button></td></tr>');
                   }
                   if (value.monto == null) {
                     montoTotal+=0;
@@ -76,15 +103,15 @@ $(document).on('click','#mostrar-detalle',function(){
               });
               $('#depositoDetalleTabla').append("<tr><td></td><td></td><td></td><td> </td><td></td><td></td><td></td><td></td><td>"+ montoTotal +"</td><td></td><td></td><td></td></tr>");
 
-          request.done(function( msg ) {
-            // $( "#log" ).html( msg );
-            console.log(msg);
-          });
-
-          request.fail(function( jqXHR, textStatus ) {
-            console.log(jqXHR.responseText,textStatus);
-            alert( "Request failed: " + textStatus + jqXHR.responseText);
-          });
+          // request.done(function( msg ) {
+          //   // $( "#log" ).html( msg );
+          //   console.log(msg);
+          // });
+          //
+          // request.fail(function( jqXHR, textStatus ) {
+          //   console.log(jqXHR.responseText,textStatus);
+          //   alert( "Request failed: " + textStatus + jqXHR.responseText);
+          // });
       });
     }
   });
