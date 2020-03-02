31df008d7f4eb6cfb9ead9286b8ab5da416e2115
jhonmc-eng <cyber_2406@hotmail.com>
2020-03-02 09:56:18 -0500

    ultimo

--- public/js/comision-venta.js
+++ public/js/comision-venta.js
@@ -292,23 +292,21 @@ $(document).ready(function(){
 
     $('#exportar').on('click',function(){
         
-        var año,mes,sucursal,vendedor;
-        año = $('#gestion').val();
+        var year,mes,sucursal,vendedor;
+        year = $('#gestion').val();
         mes = $('#mes').val();
         sucursal = $('#sucursal').val();
         vendedor = $('#vendedor').val();
 
-        if(año != '' && mes != '' && sucursal != '' && vendedor !=''){
-            console.log(año);
+        if(year != '' && mes != '' && sucursal != '' && vendedor !=''){
+            console.log(year);
             console.log(mes);
             console.log(sucursal);
             console.log(vendedor);
             //window.open( 'reporte-prosegur-resumen/'+data.fecha1+'/'+data.fecha2+'/',"_blank").focus();
-            window.open('reporte-comision/'+año+'/'+mes+'/'+sucursal+'/'+vendedor+'/',"_blank").focus();
+            window.open( 'reporte-comision/'+year+'/'+mes+'/'+sucursal+'/'+vendedor+'/',"_blank").focus();
             //window.open().focus();
             //window.open().focus();
-            
-
         }
         else{
             alert('Complete todos los campos');
