05b635732a65957572b0600ae1cc441ad7649096
= <=>
2020-03-03 11:10:00 -0500

    merge

--- app/Http/Controllers/ContenedorController.php
+++ app/Http/Controllers/ContenedorController.php
@@ -22,7 +22,7 @@ class ContenedorController extends Controller
                                       ISNULL(com_telefono,'') as com_telefono,
                                       ISNULL(com_movil,'') as com_movil,
                                       ISNULL(com_fax,'') as com_fax,
-                                      ISNULL(com_email,'') as com_email 
+                                      ISNULL(com_email,'') as com_email
                                       FROM ADM_PROVEEDOR");
       return response()->json([
 
@@ -31,9 +31,12 @@ class ContenedorController extends Controller
       ]);
     }
 
-    //return view('gestion.pagos');
 
 
+
+  }
+  public function pagos( ) {
+    return view('gestion.pagos');
 
   }
   public function parametros() {
